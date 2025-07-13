eb985fe790581d5be9eb884600f4f70eb7daf801
xmartynrichu <xmartynrichu@gmail.com>
2025-07-14 00:45:34 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,33 +1,27 @@
-// app.js
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
-const connectDB =require('./DB/dbconnection');
+const connectDB = require('./DB/dbconnection');
 const dotenv = require('dotenv');
 const user = require('./model/user');
 const PORT = process.env.PORT || 3000;
+const URL = process.env.URL;
 
-
+dotenv.config();
 connectDB();
 
-
 const app = express();
-app.use(express.json());
-// Middleware
-app.use(helmet());
+
+
 app.use(cors());
+app.use(express.json());
 app.use(bodyParser.json());
+app.use(helmet());
 app.use(morgan('dev'));
 
 // Routes
-
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
-
-
 app.get('/api/newuser', async (req, res) => {
   try {
     const users = await user.find();
@@ -38,16 +32,15 @@ app.get('/api/newuser', async (req, res) => {
   }
 });
 
-
-
 app.post('/api/savenewuser', async (req, res) => {
   try {
     const users = await user.insertOne(req.body);
     res.json(users);
   } catch (err) {
-    console.error('Error fetching users:', err);
-    res.status(500).json({ error: 'Server error while fetching users' });
+    console.error('Error saving user:', err);
+    res.status(500).json({ error: 'Server error while saving user' });
   }
 });
 
+app.listen(3000, () => console.log('Server running'));
 module.exports = app;
